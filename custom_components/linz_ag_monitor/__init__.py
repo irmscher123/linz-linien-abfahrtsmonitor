6e7db3b62c09756579d2f7c73fc0fd3704f5985b
Irmscher <irmsch@gmail.com>
2026-05-04 20:54:03 +0200

    Simplify async_setup_entry for linz_ag_monitor
    Refactor async_setup_entry to remove database blocking during startup.

--- custom_components/linz_ag_monitor/__init__.py
+++ custom_components/linz_ag_monitor/__init__.py
@@ -1,20 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .gtfs_helper import GTFSHelper
 
 DOMAIN = "linz_ag_monitor"
 PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    stop_id = entry.data.get("stop_id")
-    name = entry.data.get("name")
-    
-    helper = GTFSHelper(hass, stop_id, name)
-    await helper.update_database_if_needed()
-    
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = helper
-
+    """Setup ohne Datenbank-Blockade beim Start."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
